00c18bb6d06586436075026b44cf2dd67e18b73a
yesewhareg <hanas5294@gmail.com>
2023-06-25 13:23:43 -0700

    tests

---
+++ tests/001.m
@@ -0,0 +1,12 @@
+push 0 push 0 onto the stack
+push 1 push 1 onto the stack
+push 2
+   push 3
+                        pall
+
+push hola
+
+   push 5
+     push 6
+
+pall This is the end of our program. Monty is awesome!
