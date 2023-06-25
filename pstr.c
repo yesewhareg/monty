ef26e56c2b627fc9833c8a3d24d1a37d2fd236ae
yesewhareg <hanas5294@gmail.com>
2023-06-25 10:54:51 -0700

    Stacks, Queues - LIFO, FIFO

---
+++ pstr.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * f_pstr - Prints the string starting at the ttop of the stack,
+ * followed by a new
+ * @h: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_pstr(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void) counter;
+
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
---
+++ queue.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+/**
+ * f_queue - prints the top
+ * @h: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - add node to the tail stack
+ * @n: new_value
+ * @h: head of the stack
+ * Return: no return
+ */
+void addueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		while (aux->nect)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
